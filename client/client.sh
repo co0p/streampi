1d7e53eb3b1d300ba244218b753548d494f09718
co0p <julian.godesa@googlemail.com>
2013-02-04 11:14:44 +0100

    basic working system, ajax rocks

--- client/client.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PORT=8001
-
-echo -e "starting to serve the website..."
-python -m SimpleHTTPServer $PORT
--- server/streampi.py
+++ server/streampi.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 
-from bottle import route, run, error, hook, response
-import cPickle, json, ConfigParser
+from bottle import route, run, hook, response, static_file, error
+import cPickle, ConfigParser
 import pygst
 pygst.require("0.10")
-import gst, gobject
-
-
+import gst
 
 #----------------------------------------------
 # settings 
@@ -14,12 +12,12 @@ import gst, gobject
 
 # holds the settings
 config = None
+streamlist = None
+prefix = '/api'
+
 
 # this is the gstreamer playbin
 player = gst.element_factory_make("playbin", "player")
-
-
-
 
 # we need this hook for 'Access-Control-Allow-Origin'
 @hook('after_request')
@@ -28,10 +26,20 @@ def enable_cors():
 
 
 
-
 #----------------------------------------------
 # load / save stream list to file
 #----------------------------------------------
+
+def resetStreamInFile(path):
+	global streamlist
+
+	streamlist = {'chillout':'http://listen.di.fm/public3/chillout.pls',
+		'dradio':'http://dradio_mp3_dlf_m.akacast.akamaistream.net/7/249/142684/v1/gnl.akacast.akamaistream.net/dradio_mp3_dlf_m'}
+	writeStreamToFile(path)
+	loadStreamsFromFile(path)
+
+#def
+
 
 # load settings from cfg file
 def loadConfigFromFile(path):
@@ -74,66 +82,58 @@ def writeStreamToFile(path):
 
 
 #----------------------------------------------
-# json response helper
-#----------------------------------------------
-# every response is a json object
-def jsonResponse(code, message, data = None):
-	if data is None:
-		return {'status':code, 'message':message}
-	else:
-		jsondata = json.dumps(data)
-		return {'status':code, 'message':message, 'data':jsondata}
-
-
-#----------------------------------------------
 # data manipulation 
 #----------------------------------------------
 
+
+@route(prefix + '/reset')
+def resetData():
+	resetStreamInFile(config.get("streampi", "path"))
+#def
+
 # returns a list of streams
-@route('/get', method='GET')
+@route(prefix + '/get', method='GET')
 def getList():
-	return jsonResponse(200, "", streamlist)
+	return {'error':False, 'streams':streamlist}
 
 
 # returns a list of streams
-@route('/get/<name>')
-def getList(name):
+@route(prefix + '/get/<name>')
+def getName(name):
 
 	if name is not None and len(name) > 0:
-
 		if name in streamlist:
-			return jsonResponse
-	(200, "", streamlist[name])
+			return {'error':False, 'stream':streamlist[name]}
 		
-	return jsonResponse(500, "couldn't find stream")
+	return {'error':True, 'message':"couldn't find stream"}
 
 
 
 # adds a station to the list, overrides existing entries
-@route('/add/<name>/<url:re:.+>')
+@route(prefix + '/add/<name>/<url:re:.+>')
 def add(name=None, url=None):
 
 	if name is None or len(name) < 1:
-		return jsonResponse(500, "name missing")
+		return {'error':True, 'message':"name missing"}
 
 	if url is None or len(url) < 1:
-		return jsonResponse(500, "url missing")
+		return {'error':True, 'message':"url missing"}
 
 	streamlist[name] = url
-	writeStreamToFile(PATH)
-	return jsonResponse(200, "added stream '" +name+ "' to list")
+	writeStreamToFile(config.get("streampi", "path"))
+	return {'error':False, 'message':"added stream '" +name+ "' to list"}
 
 
 
 # removes a station from the list
-@route('/delete/<name>')
+@route(prefix + '/delete/<name>')
 def delete(name):
 
 	if name is not None and name in streamlist:
 		del streamlist[name]
-		return jsonResponse(200, "deleted '" +name+ "'stream")
+		return {'error':False, 'message':"deleted '" +name+ "'stream"}
 
-	return jsonResponse(500, "couldn't delete stream")
+	return {'error':True, 'message':"couldn't delete stream"}
 
 
 #----------------------------------------------
@@ -141,12 +141,12 @@ def delete(name):
 #----------------------------------------------
 
 # play a stream
-@route('/play')
+@route(prefix + '/play')
 def play():
-	return jsonResponse("500", "missing name of stream")
+	return {'error':True, 'message':"missing name of stream"}
 
-@route('/play/<name>')
-def play(name):
+@route(prefix + '/play/<name>')
+def playName(name):
 
 	if name in streamlist:
 
@@ -162,40 +162,45 @@ def play(name):
 
 		#start playing
 		player.set_state(gst.STATE_PLAYING)
-		return jsonResponse(200, "start playing")
+		return {'error':False, 'message':"start playing"}
 
 	else:
-		return jsonResponse(500, "missing stream")
+		return {'error':True, 'message':"missing stream"}
 
 
 
 # stop playing stream
-@route('/stop')
+@route(prefix + '/stop')
 def stop():
 
 	global player
 	player.set_state(gst.STATE_NULL)
-	return jsonResponse("200", "stop playing")
+	return {'error':False, 'message':"stop playing"}
 
+
+#----------------------------------------------
+# show the website
+#----------------------------------------------
+@route('/')
+def showWebsite():
+    return static_file('mobile.html', root='../client')
+
+@error(500)
+def onError500(code):
+	return "Server failure"
+
+
+@error(404)
+def onError404(code):
+	return "this page does not exists"
 
 #----------------------------------------------
 # main
 #----------------------------------------------
+loadConfigFromFile("streampi.cfg")
+loadStreamsFromFile(config.get("streampi", "path"))
 
-def main() :
-
-	# load config
-	loadConfigFromFile("streampi.cfg")
-
-	# parse resourcelist
-	loadStreamsFromFile(config.get("streampi", "path"))
-
-	# run webserver
-	run(host='localhost',  
+run(host='192.168.0.41',
 		port=config.get("streampi","port"),  
-		debug=config.get("streampi", "debug"))
-
-#
-if __name__ == '__main__':
-	main()
-
+		debug=config.get("streampi", "debug"),
+		reloader=True)
